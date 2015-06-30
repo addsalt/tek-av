3722297536469f86020d4cf6aa7e240f45733674
Daniela <just@addsalt.ca>
2015-06-29 21:24:35 -0400

    refactor(css): refine all styles

--- js/toggle.js
+++ js/toggle.js
@@ -1,86 +1,46 @@
-// Toggle Nav JS
+// Flipper 1
 
-var donateTab = document.querySelector('#donate-tab');
-	
-	document.querySelector('.donate-btn').addEventListener('click', function(e){
-		e.preventDefault(); 
-	
-	
-		if (donateTab.getAttribute('data-state') == 'expanded'){
-			
-			donateTab.setAttribute('data-state', 'collapsed');
-		
-		}else{
-		
-			donateTab.setAttribute('data-state', 'expanded');
-		
-		}
-	});
+$(function() {
+    $( "#front-1" ).click(function() {
+      $( "#flipper-1" ).addClass( "flipped", 1000 );
+    });
+  });
 
-var volunteerTab = document.querySelector('#volunteer-tab');
-	
-	document.querySelector('.volunteer-btn').addEventListener('click', function(e){
-		e.preventDefault(); 
-	
-	
-		if (volunteerTab.getAttribute('data-state') == 'expanded'){
-			
-			volunteerTab.setAttribute('data-state', 'collapsed');
-		
-		}else{
-		
-			volunteerTab.setAttribute('data-state', 'expanded');
-		
-		}
-	});
+$(function() {
+    $( "#back-1" ).click(function() {
+      $( "#flipper-1" ).removeClass( "flipped", 1000 );
+    });
+  });
 
-var seeMore = document.querySelector('.see-more');
-	
-	document.querySelector('.see-more-btn').addEventListener('click', function(e){
-		e.preventDefault(); 
-	
-	
-		if (seeMore.getAttribute('data-state') == 'expanded'){
-			
-			seeMore.setAttribute('data-state', 'collapsed');
-		
-		}else{
-		
-			seeMore.setAttribute('data-state', 'expanded');
-		
-		}
-	});
 
-var seeMoreTwo = document.querySelector('.see-more-2');
-	
-	document.querySelector('.see-more-btn-2').addEventListener('click', function(e){
-		e.preventDefault(); 
-	
-	
-		if (seeMoreTwo.getAttribute('data-state') == 'expanded'){
-			
-			seeMoreTwo.setAttribute('data-state', 'collapsed');
-		
-		}else{
-		
-			seeMoreTwo.setAttribute('data-state', 'expanded');
-		
-		}
-	});
+// Flipper 2
 
-var seeMoreThree = document.querySelector('.see-more-3');
-	
-	document.querySelector('.see-more-btn-3').addEventListener('click', function(e){
-		e.preventDefault(); 
-	
-	
-		if (seeMoreThree.getAttribute('data-state') == 'expanded'){
-			
-			seeMoreThree.setAttribute('data-state', 'collapsed');
-		
-		}else{
-		
-			seeMoreThree.setAttribute('data-state', 'expanded');
-		
-		}
-	});
+$(function() {
+    $( "#front-2" ).click(function() {
+      $( "#flipper-2" ).addClass( "flipped", 1000 );
+    });
+  });
+
+$(function() {
+    $( "#back-2" ).click(function() {
+      $( "#flipper-2" ).removeClass( "flipped", 1000 );
+    });
+  });
+
+
+// Flipper 3
+
+$(function() {
+    $( "#front-3" ).click(function() {
+      $( "#flipper-3" ).addClass( "flipped", 1000 );
+    });
+  });
+
+$(function() {
+    $( "#back-3" ).click(function() {
+      $( "#flipper-3" ).removeClass( "flipped", 1000 );
+    });
+  });
+
+
+
